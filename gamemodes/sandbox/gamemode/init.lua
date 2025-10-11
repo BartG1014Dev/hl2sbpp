15cc567c29d122ccde9dcab933525cf10bab90ad
ThePixelMoon <yourlocalmoon7@gmail.com>
2025-10-11 20:27:45 +0300

    feat(gamemodes): add sbox_weapons cvar

--- gamemodes/sandbox/gamemode/init.lua
+++ gamemodes/sandbox/gamemode/init.lua
@@ -17,6 +17,9 @@ local tSpawnPointClassnames = {
   "info_player_start",
 }
 
+local FCVAR_REPLICATED = _E.FCVAR.REPLICATED
+local sbox_weapons = ConVar("sbox_weapons", "1", FCVAR_REPLICATED)
+
 function GM:AddLevelDesignerPlacedObject(pEntity)
   return false
 end
@@ -24,54 +27,56 @@ end
 function GM:PlayerSpawn(pPlayer) end
 
 function GM:GiveDefaultItems(pPlayer)
-  -- give basically everything
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Pistol")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "SMG1")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "smg1_grenade")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "grenade")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Buckshot")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 32, "357")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "AR2")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 5, "AR2AltFire")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "rpg_round")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "slam")
+  if sbox_weapons:GetBool() then
+    -- give basically everything
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Pistol")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "SMG1")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "smg1_grenade")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "grenade")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Buckshot")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 32, "357")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "AR2")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 5, "AR2AltFire")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "rpg_round")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "slam")
 
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "9mmRound")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 32, "357Round")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 10, "MP5_Grenade")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "RPG_Rocket")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Uranium")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Hornet")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Snark")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "TripMine")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 3, "Satchel")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "12mmRound")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "BuckshotHL1")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 255, "XBowBoltHL1")
-  _R.CBasePlayer.GiveAmmo(pPlayer, 5, "GrenadeHL1")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "9mmRound")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 32, "357Round")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 10, "MP5_Grenade")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "RPG_Rocket")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Uranium")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Hornet")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "Snark")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "TripMine")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 3, "Satchel")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "12mmRound")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "BuckshotHL1")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 255, "XBowBoltHL1")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 5, "GrenadeHL1")
 
-  _R.CBasePlayer.GiveAmmo(pPlayer, 30, "Medkit")
+    _R.CBasePlayer.GiveAmmo(pPlayer, 30, "Medkit")
 
-  pPlayer:GiveNamedItem("weapon_crowbar")
-  --pPlayer:GiveNamedItem( "weapon_stunstick" )
-  pPlayer:GiveNamedItem("weapon_pistol")
-  pPlayer:GiveNamedItem("weapon_357")
-  pPlayer:GiveNamedItem("weapon_smg1")
-  pPlayer:GiveNamedItem("weapon_ar2")
-  pPlayer:GiveNamedItem("weapon_shotgun")
-  pPlayer:GiveNamedItem("weapon_frag")
-  pPlayer:GiveNamedItem("weapon_crossbow")
-  pPlayer:GiveNamedItem("weapon_rpg")
-  --pPlayer:GiveNamedItem( "weapon_slam" )
-  pPlayer:GiveNamedItem("weapon_physcannon")
-  --pPlayer:GiveNamedItem( "weapon_bugbait" )
-  pPlayer:GiveNamedItem("weapon_toolgun")
-  pPlayer:GiveNamedItem("weapon_physgun")
+    pPlayer:GiveNamedItem("weapon_crowbar")
+    --pPlayer:GiveNamedItem( "weapon_stunstick" )
+    pPlayer:GiveNamedItem("weapon_pistol")
+    pPlayer:GiveNamedItem("weapon_357")
+    pPlayer:GiveNamedItem("weapon_smg1")
+    pPlayer:GiveNamedItem("weapon_ar2")
+    pPlayer:GiveNamedItem("weapon_shotgun")
+    pPlayer:GiveNamedItem("weapon_frag")
+    pPlayer:GiveNamedItem("weapon_crossbow")
+    pPlayer:GiveNamedItem("weapon_rpg")
+    --pPlayer:GiveNamedItem( "weapon_slam" )
+    pPlayer:GiveNamedItem("weapon_physcannon")
+    --pPlayer:GiveNamedItem( "weapon_bugbait" )
+    pPlayer:GiveNamedItem("weapon_toolgun")
+    pPlayer:GiveNamedItem("weapon_physgun")
 
-  -- uhh im a shitty coder yes
-  local pWeapon = pPlayer:Weapon_OwnsThisType("weapon_physgun")
-  if pWeapon then
-    pPlayer:Weapon_Switch(pWeapon, 0)
+    -- uhh im a shitty coder yes
+    local pWeapon = pPlayer:Weapon_OwnsThisType("weapon_physgun")
+    if pWeapon then
+      pPlayer:Weapon_Switch(pWeapon, 0)
+    end
   end
 end
 
