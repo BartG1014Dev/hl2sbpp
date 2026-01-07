60cafec84bfd0862e52a0ba9c758af69a5b18a8a
BartG1014 <idxrpp@gmail.com>
2026-01-08 02:24:32 +0300

    Fix inputs in lua/api
    These numbers are correct, and I tested them.

--- lua/api/in.lua
+++ lua/api/in.lua
@@ -3,77 +3,77 @@
 IN = {}
 
 ---Primary attack (e.g., left mouse button)
-IN.ATTACK = 0
+IN.ATTACK = 1
 
 ---Jump action
-IN.JUMP = 0
+IN.JUMP = 2
 
 ---Duck / crouch action
-IN.DUCK = 0
+IN.DUCK = 4
 
 ---Move forward
-IN.FORWARD = 0
+IN.FORWARD = 8
 
 ---Move backward
-IN.BACK = 0
+IN.BACK = 16
 
 ---Use / interact with objects
-IN.USE = 0
+IN.USE = 32
 
 ---Cancel current action
-IN.CANCEL = 0
+IN.CANCEL = 64
 
 ---Move left
-IN.LEFT = 0
+IN.LEFT = 128
 
 ---Move right
-IN.RIGHT = 0
+IN.RIGHT = 256
 
 ---Strafe left
-IN.MOVELEFT = 0
+IN.MOVELEFT = 512
 
 ---Strafe right
-IN.MOVERIGHT = 0
+IN.MOVERIGHT = 1024
 
 ---Secondary attack (e.g., right mouse button)
-IN.ATTACK2 = 0
+IN.ATTACK2 = 2048
 
 ---Run / sprint
-IN.RUN = 0
+IN.RUN = 4096
 
 ---Reload weapon
-IN.RELOAD = 0
+IN.RELOAD = 8192
 
 ---Alternative action 1
-IN.ALT1 = 0
+IN.ALT1 = 16384
 
 ---Alternative action 2
-IN.ALT2 = 0
+IN.ALT2 = 32768
 
 ---Show scoreboard
-IN.SCORE = 0
+IN.SCORE = 65536
 
 ---Enable speed modifier
-IN.SPEED = 0
+IN.SPEED = 131072
 
 ---Walk slowly
-IN.WALK = 0
+IN.WALK = 262144
 
 ---Zoom / aim down sights
-IN.ZOOM = 0
+IN.ZOOM = 524288
 
 ---Select weapon slot 1
-IN.WEAPON1 = 0
+IN.WEAPON1 = 1048576
 
 ---Select weapon slot 2
-IN.WEAPON2 = 0
+IN.WEAPON2 = 2097152
 
 ---Bullrush action
-IN.BULLRUSH = 0
+IN.BULLRUSH = 4194304
 
 ---Primary grenade
-IN.GRENADE1 = 0
+IN.GRENADE1 = 8388608
 
 ---Secondary grenade
-IN.GRENADE2 = 0
+IN.GRENADE2 = 16777216
 return IN
